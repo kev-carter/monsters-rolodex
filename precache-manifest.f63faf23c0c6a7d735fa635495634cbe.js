50d582b38f39cfbf6b0e79352baaf85c72644ee2
Carter <AA36963@ad.wellpoint.com>
2020-04-20 15:52:08 -0400

    Updates

---
+++ precache-manifest.f63faf23c0c6a7d735fa635495634cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19780b0ff44bef6a6363f8fb5a607b6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a945e7ff66a1e4796e9c",
+    "url": "/monsters-rolodex/static/css/main.3a27de24.chunk.css"
+  },
+  {
+    "revision": "abd7d71b0a1f3859dfb8",
+    "url": "/monsters-rolodex/static/js/2.0adb66df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0adb66df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a945e7ff66a1e4796e9c",
+    "url": "/monsters-rolodex/static/js/main.72a4c4c2.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
